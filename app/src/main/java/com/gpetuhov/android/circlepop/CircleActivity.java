39975623fd500ccdd3ee7ce6eaacbd76f3d7c875
Gennadiy Petuhov <g3pmail@gmail.com>
2016-05-20 15:22:15 +0400

    Create Android Studio project

---
+++ app/src/main/java/com/gpetuhov/android/circlepop/CircleActivity.java
@@ -0,0 +1,13 @@
+package com.gpetuhov.android.circlepop;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CircleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_circle);
+    }
+}
